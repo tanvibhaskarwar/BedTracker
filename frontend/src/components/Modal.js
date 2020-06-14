ae65edd76b5303817d9a4fe702b0e00c91fca8e2
tanvibhaskarwar <tanvi.bhaskarwar@gmail.com>
2020-06-14 12:43:08 +0530

    Modal Content

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -16,11 +16,16 @@ export class HospModal extends Component {
       >
         <Modal.Header closeButton>
           <Modal.Title align="center" id="contained-modal-title-vcenter">
-            <h1>     What do we offer under Testing</h1>
+            <h1> What do we offer under Testing</h1>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          Helloo
+          Testing being the first stage towards fighting Covid-19, we aim at
+          providing a trouble-free testing provision, where you can book a test
+          in advance by coming onto our site. A referal code will be provided
+          after consulting with the doctor which will hint you to book a test on
+          our portal! Our services aim at rendering a hassle-free and
+          easily-deciphered procedure towards fighting this virus with unison!
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={this.props.onHide}>Close</Button>
